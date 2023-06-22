39eb74570feb6b707b78a14b1f778fd0c8d44302
Jared Valenzuela <jared.valenzuela@epn.edu.ec>
2023-06-21 22:30:55 -0500

    Creacion de clases Gato y Puma para heredar

--- src/Gato.java
+++ src/Gato.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class Gato {
+public class Gato extends Felinos {
+    private String pelaje;
+    private double peso;
+    private String color;
+    private String nivel_actividad;
+    //Metodo super
+    public Gato() {super();
+    }
+    //Constructores
+    public Gato(String raza, String tamano, String edad, String habitat,String pelaje, double peso, String color, String nivel_actividad) {
+        this.pelaje = pelaje;
+        this.peso = peso;
+        this.color = color;
+        this.nivel_actividad = nivel_actividad;
+    }
+
+    //Getters and Setters
+    public String getPelaje() {
+        return pelaje;
+    }
+
+    public void setPelaje(String pelaje) {
+        this.pelaje = pelaje;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getNivel_actividad() {
+        return nivel_actividad;
+    }
+
+    public void setNivel_actividad(String nivel_actividad) {
+        this.nivel_actividad = nivel_actividad;
+    }
+
+    public void sentidos(){
+        System.out.println("Este es el metodo sentido que pertenece a Gato");
+    }
+    public void comunicacion(){
+        System.out.println("Este es el metodo comunicacion que pertenece a Gato");
+    }
+
 }
--- src/Puma.java
+++ src/Puma.java
@@ -1,2 +1,58 @@
-package PACKAGE_NAME;public class Puma {
+public class Puma extends Felinos{
+    private String dieta;
+    private double velocidad;
+    private String tipo_puma;
+    private double fuerza_mordedura;
+    //Metodo super
+    public Puma() {super();
+    }
+    //Constructores
+    public Puma(String raza, String tamano, String edad, String habitat,String dieta, double velocidad, String tipo_puma, double fuerza_mordedura) {
+        this.dieta = dieta;
+        this.velocidad = velocidad;
+        this.tipo_puma = tipo_puma;
+        this.fuerza_mordedura = fuerza_mordedura;
+    }
+    //Setters and Getters
+
+    public String getDieta() {
+        return dieta;
+    }
+
+    public void setDieta(String dieta) {
+        this.dieta = dieta;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public String getTipo_puma() {
+        return tipo_puma;
+    }
+
+    public void setTipo_puma(String tipo_puma) {
+        this.tipo_puma = tipo_puma;
+    }
+
+    public double getFuerza_mordedura() {
+        return fuerza_mordedura;
+    }
+
+    public void setFuerza_mordedura(double fuerza_mordedura) {
+        this.fuerza_mordedura = fuerza_mordedura;
+    }
+
+    public void reproduccion (){
+        System.out.println("Este es el metodo reproduccion que pertenece a Puma");
+
+    }
+
+    public void mitologia(){
+        System.out.println("Este es el metodo mitologia que pertenece a Puma");
+    }
 }
